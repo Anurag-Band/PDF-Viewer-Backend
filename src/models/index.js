a1e46365bd1717243bac7592736946c82d50b9a0
Anurag-Band <anuragband10@gmail.com>
2023-02-09 18:12:29 +0530

    deployment config added

--- src/models/index.js
+++ src/models/index.js
@@ -5,7 +5,7 @@ require("dotenv").config({
 const { Sequelize, DataTypes, Op } = require("sequelize");
 const sequelize = new Sequelize(
   process.env.DB,
-  "anurag",
+  "freedb_pdf-viewer-user", // Hard Coded because it's not taking from .env
   process.env.PASSWORD,
   {
     host: process.env.HOST,
