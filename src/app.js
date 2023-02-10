b83ca7f91609e83ce48854ffcaff061f1346238e
Anurag-Band <anuragband10@gmail.com>
2023-02-10 14:57:05 +0530

    added allowed origins on cors

--- src/app.js
+++ src/app.js
@@ -7,7 +7,11 @@ const cookieParser = require("cookie-parser");
 
 app.use(
   cors({
-    origin: "*",
+    origin: [
+      "http://localhost:3001",
+      "pdf-viewer-mysql-aws-s3.netlify.app",
+      "https://production--pdf-viewer-mysql-aws-s3.netlify.app",
+    ],
     credentials: true,
   })
 );
